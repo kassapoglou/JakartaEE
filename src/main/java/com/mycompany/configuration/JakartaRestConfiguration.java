5820e98ab0930ce01b593769791bb7f776c99d40
Michail <Michail@192.168.1.7>
2025-03-22 22:01:07 +0200

    JakartaEE_Lesson9_CustomQualifiers

---
+++ src/main/java/com/mycompany/configuration/JakartaRestConfiguration.java
@@ -0,0 +1,9 @@
+package com.mycompany.configuration;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/com/mycompany/controllers/Controller.java
@@ -0,0 +1,123 @@
+package com.mycompany.controllers;
+
+import com.mycompany.entities.Department;
+import com.mycompany.entities.Employee;
+import com.mycompany.services.DepartmentAssignment;
+import com.mycompany.services.ITAssignment;
+import jakarta.annotation.Resource;
+import jakarta.enterprise.inject.Default;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.HeuristicMixedException;
+import jakarta.transaction.HeuristicRollbackException;
+import jakarta.transaction.NotSupportedException;
+import jakarta.transaction.RollbackException;
+import jakarta.transaction.SystemException;
+import jakarta.transaction.UserTransaction;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@WebServlet(name = "Controller", urlPatterns = {"/Controller"})
+public class Controller extends HttpServlet {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Resource
+    private UserTransaction userTransaction;
+
+    @Inject
+    @Default
+    @ITAssignment
+    DepartmentAssignment departmentAssignment;
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        Long dept_id = null;
+
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String email = request.getParameter("email");
+        String date = request.getParameter("hireDate");
+        String phoneNumber = request.getParameter("phoneNumber");
+        Double monthlySalary = Double.valueOf(request.getParameter("monthlySalary"));
+        String dept = request.getParameter("department");
+
+        Department department = new Department();
+        Employee employee = new Employee();
+
+        if ("None".equals(dept)) {
+            department.setDept_id(departmentAssignment.generateDepartment());
+        } else {
+            switch (dept) {
+                case "HR" ->
+                    department.setDept_id(1L);
+                case "IT" ->
+                    department.setDept_id(2L);
+                case "Finance" ->
+                    department.setDept_id(3L);
+                case "Marketing" ->
+                    department.setDept_id(4L);
+                default -> {
+                }
+            }
+        }
+
+        employee.setFirstName(firstName);
+        employee.setLastName(lastName);
+        employee.setMonthlySalary(monthlySalary);
+        Date dt = null;
+        try {
+            dt = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+        } catch (ParseException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        employee.setHireDate(dt);
+        employee.setEmail(email);
+        employee.setPhoneNumber(phoneNumber);
+        employee.setDepartment(department);
+
+        try {
+            userTransaction.begin();
+            em.persist(employee);
+            userTransaction.commit();
+        } catch (NotSupportedException
+                | SystemException | RollbackException
+                | HeuristicMixedException | HeuristicRollbackException
+                | SecurityException | IllegalStateException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet NewServlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>The user " + employee.getFirstName() + " has been registered </h1>");
+        out.println("</body>");
+        out.println("</html>");
+
+    }
+}
