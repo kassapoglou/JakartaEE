879dc7fd26cbe51184cb875723c94d04689c7fc1
Michail <Michail@192.168.1.7>
2025-03-22 21:12:36 +0200

    JakartaEE_Lesson7_ManyToOneMapping

---
+++ src/main/java/com/mycompany/entities/Department.java
@@ -0,0 +1,52 @@
+package com.mycompany.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import java.util.List;
+
+
+@Entity
+@Table(name = "DEPARTMENTS")
+public class Department implements Serializable {
+
+    public Department() {
+    }
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    private Long dept_id;
+    
+    @Column(name = "dept_name")
+    private String name;
+    
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
+
+    public Long getDept_id() {
+        return dept_id;
+    }
+
+    public void setDept_id(Long dept_id) {
+        this.dept_id = dept_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
